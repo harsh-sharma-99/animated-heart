a325ea35fd8f40b02e75c338b5543e5ae196b440
Harsh Sharma <harsh.sharma@rooter.io>
2022-06-21 14:02:54 +0530

    added animation

---
+++ src/component/heartComponent/heart.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+import "./styles.css"
+
+const Heart = () => {
+  useEffect(() => {
+    init();
+  }, [])
+
+  const init = ( ) => {
+    
+  }
+  return (
+    <div className = 'heart'></div>
+  )
+}
+
+export default Heart
